38836809acc0ec2f4204f690a085bfb40fb60441
Us <k.konopka2008@gmail.com>
2024-09-17 23:43:21 +0200

    Add files via upload

---
+++ Informatyka (Java)/src/ScannerMain.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ScannerMain {
+    public static void main(String[] args){
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe: ");
+        int l1 = klawiatura.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        int l2 = klawiatura.nextInt();
+
+        if (l1 < l2)
+            System.out.println("Los 1 jest najmniejszy");
+        else if (l2 < l1)
+            System.out.println("Los 2 jest najmniejszy");
+
+
+    }
+}
